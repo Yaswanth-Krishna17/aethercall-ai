ad5359ef9a6bd25eccb0cd4fad0891cdd5e5396f
Yaswanth Krishna <yaswanthkrishna17@gmail.com>
2026-05-29 12:55:35 +0530

    Force IPv4 first dns resolution to avoid ENETUNREACH on Render

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import 'dotenv/config';
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
